574809710e50c2a268dc28b5dd5050e31e66efb5
Andrei M <gagarin880120@gmail.com>
2020-06-21 13:22:42 +0300

    fix: change some styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,14 +7,16 @@ import SearchField from '../SearchField/SearchField';
 export default function NavBar() {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.buttonsContainer}>
-        <TemperatureScaleButton
-          scale="celsius"
-        />
-        <TemperatureScaleButton
-          scale="fahrenheit"
-        />
+      <div className={styles.container}>
         <LanguageSelect />
+        <div className={styles.scaleButtons}>
+          <TemperatureScaleButton
+            scale="celsius"
+          />
+          <TemperatureScaleButton
+            scale="fahrenheit"
+          />
+        </div>
       </div>
       <SearchField />
     </div>
