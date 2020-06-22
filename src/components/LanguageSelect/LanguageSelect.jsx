d6bf6c75fe68304542ee5ba46900c9e3865d2a96
Andrei M <gagarin880120@gmail.com>
2020-06-22 11:22:30 +0300

    fix: change selected prop with defaultValue

--- src/components/LanguageSelect/LanguageSelect.jsx
+++ src/components/LanguageSelect/LanguageSelect.jsx
@@ -4,7 +4,7 @@ import styles from './LanguageSelect.module.css';
 export default function LanguageSelect() {
   return (
     <select name="language" className={styles.select}>
-      <option value="en" selected>en</option>
+      <option value="en" defaultValue>en</option>
       <option value="ru">ru</option>
     </select>
   );
