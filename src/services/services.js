01eb21a97054f7299ee3e91f93ddb709e09b7744
Andrei M <gagarin880120@gmail.com>
2020-07-13 21:14:17 +0300

    fix: change some styles

--- src/services/services.js
+++
@@ -1,5 +0,0 @@
-// export default async function getImageSize(id) {
-//   const API_KEY = 'c8090405b9c8b36dbc1e8a34495de0cf';
-//   const url = `https://www.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${API_KEY}&photo_id=${id}&format=json&nojsoncallback=1`
-//   const res = await fetch(url)
-// }
