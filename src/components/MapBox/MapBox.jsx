7b415bb12efd6875e0124f08b1527d432905822b
Andrei M <gagarin880120@gmail.com>
2020-07-13 21:05:57 +0300

    feat: add mapZoom dispatching on map.move

--- src/components/MapBox/MapBox.jsx
+++ src/components/MapBox/MapBox.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import mapboxgl from 'mapbox-gl';
 import styles from './MapBox.module.css';
 
-export default function MapBox({ lon, lat }) {
+export default function MapBox({ lon, lat, mapZoom, onMapMove }) {
   const mapContainer = useRef();
   mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FnYXJpbjg4MDEyMCIsImEiOiJjazNicHc1NW4wOHFvM251aXg3YzlheDFmIn0.C2XPJXYAzS1e0OjVNFC-NQ';
   useEffect(() => {
@@ -11,11 +11,11 @@ export default function MapBox({ lon, lat }) {
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lon, lat],
-      zoom: 8,
+      zoom: mapZoom,
     });
-    // map.on('move', () => {
-    //   console.log(map.getZoom().toFixed(2));
-    // });
+    map.on('move', () => {
+      onMapMove(map.getZoom().toFixed(2));
+    });
   }, [mapContainer, lon, lat]);
   return (
     <div ref={mapContainer} className={styles.container} />
@@ -25,9 +25,13 @@ export default function MapBox({ lon, lat }) {
 MapBox.propTypes = {
   lon: PropTypes.number,
   lat: PropTypes.number,
+  mapZoom: PropTypes.string,
+  onMapMove: PropTypes.func,
 };
 
 MapBox.defaultProps = {
   lon: 0,
   lat: 0,
+  mapZoom: '8',
+  onMapMove: null,
 };
--- src/components/MapBox/MapBoxContainer.jsx
+++ src/components/MapBox/MapBoxContainer.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import MapBox from './MapBox';
-import { selectLatitude, selectLongitude } from '../../redux/selectors';
+import { selectLatitude, selectLongitude, selectMapZoom } from '../../redux/selectors';
+import { setMapZoom } from '../../redux/actions';
 
-export function MapBoxContainer({ lon, lat }) {
+export function MapBoxContainer({ lon, lat, mapZoom, onMapMove }) {
   return (
     <MapBox
       lon={lon}
       lat={lat}
+      mapZoom={mapZoom}
+      onMapMove={onMapMove}
     />
   );
 }
@@ -16,16 +19,27 @@ export function MapBoxContainer({ lon, lat }) {
 export const mapStateToProps = (state) => ({
   lon: selectLongitude(state),
   lat: selectLatitude(state),
+  mapZoom: selectMapZoom(state),
+});
+
+export const mapDispatchToProps = (dispatch) => ({
+  onMapMove(zoom) {
+    dispatch(setMapZoom(zoom));
+  },
 });
 
 MapBoxContainer.propTypes = {
   lon: PropTypes.number,
   lat: PropTypes.number,
+  mapZoom: PropTypes.string,
+  onMapMove: PropTypes.func,
 };
 
 MapBoxContainer.defaultProps = {
   lon: 0,
   lat: 0,
+  mapZoom: '8',
+  onMapMove: null,
 };
 
-export default connect(mapStateToProps)(MapBoxContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(MapBoxContainer);
