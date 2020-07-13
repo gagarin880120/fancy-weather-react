fd0ffd929536f89cb90cbb65a8d363d4feeb133e
Andrei M <gagarin880120@gmail.com>
2020-07-13 21:09:04 +0300

    feat: add speechRecognition

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -2,26 +2,51 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './SearchField.module.css';
 
-export default function SearchField({ onSearch, currentDateInterval }) {
+export default function SearchField({ onSearch, currentDateInterval, language }) {
   const [query, setQuery] = useState('');
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
   return (
     <div className={styles.wrapper}>
       <input
         type="search"
         name="search"
         className={styles.searchInput}
+        value={query || ''}
         onChange={(e) => {
           setQuery(e.target.value);
         }}
         onKeyDown={(e) => {
           if (e.key === 'Enter') {
-            onSearch(query, currentDateInterval);
+            if (query) {
+              onSearch(query, currentDateInterval, language);
+            }
           }
         }}
       />
       <button
         type="button"
-        onClick={() => onSearch(query, currentDateInterval)}
+        onClick={() => {
+          recognition.start();
+          recognition.onresult = (e) => {
+            const transcript = Array.from(e.results)
+              .map((result) => result[0])
+              .map((result) => result.transcript)
+              .join('');
+            setQuery(transcript);
+            onSearch(transcript, currentDateInterval, language);
+          };
+        }}
+      >
+        Voice
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          if (query) {
+            onSearch(query, currentDateInterval, language);
+          }
+        }}
       >
         Search
       </button>
@@ -32,9 +57,11 @@ export default function SearchField({ onSearch, currentDateInterval }) {
 SearchField.propTypes = {
   onSearch: PropTypes.func,
   currentDateInterval: PropTypes.number,
-}
+  language: PropTypes.string,
+};
 
 SearchField.defaultProps = {
   onSearch: null,
   currentDateInterval: 0,
-}
+  language: 'en',
+};
